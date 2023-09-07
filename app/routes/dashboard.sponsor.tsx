910d333862b07ddac07b2191b19bca5369f975c5
Peizhe Tian <peizhe2001@hotmail.com>
2023-09-07 16:57:35 -0400

    Add previous sponsors & current sponsors section

--- app/routes/dashboard.sponsor.tsx
+++ app/routes/dashboard.sponsor.tsx
@@ -1,5 +1,7 @@
 import cgdpink from "assets/icons/cgd-transp-pink.png";
-import sponsorpacklm from "assets/pics/light mode.png";
+import sponsorpacklm from "assets/pics/light mode .png";
+import cc2mtl from "assets/icons/cc2-mtl.png";
+import cc2 from "assets/icons/cc2.png";
 import "app/css/sponsors.css";
 import type { V2_MetaFunction } from "@remix-run/node";
 
@@ -24,6 +26,16 @@ export default function DashboardSponsor() {
           developers of tomorrow!
         </div>
         <img src={sponsorpacklm} alt="concordia gamedev club sponsorship package overview light mode" className="sponsor-img" />
+        <button className="button"></button>
+        <div className="past-sponsors-container">
+          <div className="sponsor-text">Special thanks to our previous sponsors!</div>
+          <div className="past-sponsors">
+            <img src={cc2mtl} alt="concordia gamedev club's past sponsor cyberconnect2 montreal logo" className="past-sponsor-img"/>
+            <img src={cc2} alt="concordia gamedev club's past sponsor cyberconnect2 logo" className="past-sponsor-img" />
+          </div>
+        </div>
+        
+        
       </div>
 
       <div className="background">
