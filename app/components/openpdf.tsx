fb9fd75af008e32d15c5a0340d3da61697034b38
Peizhe Tian <peizhe2001@hotmail.com>
2023-09-21 21:25:18 -0400

    Fix sponsor us button initial animation

--- app/components/openpdf.tsx
+++ app/components/openpdf.tsx
@@ -1,24 +1,17 @@
+function PDFViewer() {
+  const openPDF = () => {
+    const pdfURL = "/uploads/sample.pdf";
 
+    window.open(pdfURL, "_blank");
+  };
 
-function PDFViewer(){
-
-    const openPDF = () => {
-        const pdfURL = '/uploads/sample.pdf';
-
-        window.open(pdfURL, '_blank');
-        
-    };
-
-    
-    return (
-            <div className="button-container">
-                <a onClick={openPDF} className="button"><p> Click here for the detailed sponsorship package PDF</p></a>
-            </div>
-            
-
-    );
-    
+  return (
+    <div className="button-container">
+      <a onClick={openPDF} className="button">
+        <p> Click here for the detailed sponsorship package PDF</p>
+      </a>
+    </div>
+  );
 }
 
 export default PDFViewer;
-
