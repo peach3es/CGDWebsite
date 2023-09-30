913e0e6c280be063fdd8c27a44de9f9b1f60c8bc
Peizhe Tian <peizhe2001@hotmail.com>
2023-09-30 15:06:55 -0400

    tsconfig & remix.config update

--- remix.config.js
+++ remix.config.js
@@ -1,17 +1,40 @@
+// /** @type {import('@remix-run/dev').AppConfig} */
+// export default {
+//   ignoredRouteFiles: ["**/.*"],
+//   // appDirectory: "app",
+//   // assetsBuildDirectory: "public/build",
+//   // serverBuildPath: "build/index.js",
+//   // publicPath: "/build/",
+//   serverModuleFormat: "esm",
+//   future: {
+//     v2_dev: true,
+//     v2_errorBoundary: true,
+//     v2_headers: true,
+//     v2_meta: true,
+//     v2_normalizeFormMethod: true,
+//     v2_routeConvention: true,
+//   },
+// };
+
+const baseConfig =
+  process.env.NODE_ENV === "production"
+    ? // when running the Netify CLI or building on Netlify, we want to use
+      {
+        server: "./server.js",
+        serverBuildPath: ".netlify/functions-internal/server.js",
+      }
+    : // otherwise support running remix dev, i.e. no custom server
+      undefined;
+
 /** @type {import('@remix-run/dev').AppConfig} */
-export default {
+module.exports = {
+  ...baseConfig,
   ignoredRouteFiles: ["**/.*"],
-  // appDirectory: "app",
-  // assetsBuildDirectory: "public/build",
-  // serverBuildPath: "build/index.js",
-  // publicPath: "/build/",
-  serverModuleFormat: "esm",
+  // See https://remix.run/docs/en/main/file-conventions/route-files-v2
   future: {
-    v2_dev: true,
-    v2_errorBoundary: true,
-    v2_headers: true,
-    v2_meta: true,
-    v2_normalizeFormMethod: true,
     v2_routeConvention: true,
   },
+  // add your own custom config here if you want to.
+  //
+  // See https://remix.run/docs/en/v1/file-conventions/remix-config
 };
