8cfc6e7af8fe63f4d9a8442458b9c7509a47a0a2
peaches <peizhe2001@hotmail.com>
2023-09-30 19:28:02 -0400

    Remix reset

---
+++ app/routes/_index.tsx
@@ -0,0 +1,60 @@
+import type { MetaFunction } from "@remix-run/node";
+import "app/css/homepage.css";
+import "assets/fonts/fonts.css";
+import "app/css/background-animation.css";
+import cgdpink from "assets/icons/cgd-transp-pink.png";
+
+export const meta: MetaFunction = () => {
+  return [
+    { title: "Home | CGD" },
+    { name: "description", content: "Welcome to Concordia Game Dev Club!" },
+    { icon: "./favicon.ico" },
+  ];
+};
+
+export default function Index() {
+  return (
+    <div className="header-footer">
+      <div className="page">
+        <h1 className="gameclub">Concordia Game Development</h1>
+      </div>
+      <div className="background">
+        <span>
+          <img
+            src={cgdpink}
+            alt="cgd pink alternate logo"
+            className="floaties"
+          />
+        </span>
+        <span>
+          <img
+            src={cgdpink}
+            alt="cgd pink alternate logo"
+            className="floaties"
+          />
+        </span>
+        <span>
+          <img
+            src={cgdpink}
+            alt="cgd pink alternate logo"
+            className="floaties"
+          />
+        </span>
+        <span>
+          <img
+            src={cgdpink}
+            alt="cgd pink alternate logo"
+            className="floaties"
+          />
+        </span>
+        <span>
+          <img
+            src={cgdpink}
+            alt="cgd pink alternate logo"
+            className="floaties"
+          />
+        </span>
+      </div>
+    </div>
+  );
+}
