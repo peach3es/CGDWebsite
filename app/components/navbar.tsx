e988caa2f8433d64fad0149b52fe4a150bb5d5ec
Peizhe Tian <peizhe2001@hotmail.com>
2023-10-01 16:42:57 -0400

    Start responsive navbar

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import "app/css/navbar.css";
 import { Link } from "@remix-run/react";
 import CGDLogo from "assets/icons/cgd-transparent.png";
@@ -12,98 +11,102 @@ import itch from "assets/icons/itch.svg";
 import ln from "assets/icons/linked.svg";
 
 const Navbar = () => {
+  let Links = [
+    {
+      name: "Home",
+      link: "/",
+      img: homeicon,
+      alt: "home icon that brings you back to gamedev main page",
+    },
+    {
+      name: "About",
+      link: "/about",
+      img: abouticon,
+      alt: "icon that brings you to gamedev about us page",
+    },
+    {
+      name: "Events",
+      link: "/events",
+      img: eventsicon,
+      alt: "icon that brings you to gamedev upcoming events page",
+    },
+    {
+      name: "Sponsors",
+      link: "/sponsor",
+      img: sponsorsicon,
+      alt: "icon that brings you to gamedev sponsor page",
+    },
+  ];
+
   return (
-    <div className="header">
-      <div>
+    <div className="navbar md-flex shadow-md w-full fixed top-0 left-0">
+      <div className="flex-shrink-0">
         <a href="/">
           <img
             src={CGDLogo}
             alt="Concordia-Game-Dev club logo"
-            className="cgd-logo"
+            className="cgd-logo h-16 w-16 p-3 "
           />
         </a>
       </div>
+      <div className="hidden sm:items-center sm:flex">
+        <ul className="page-links">
+          {Links.map((link) => (
+            <li key={link.name}>
+              <Link to={link.link} className="nav">
+                <img src={link.img} alt={link.alt} className="svg-icon" />
+                <h3 className="nav-item">{link.name}</h3>
+              </Link>
+            </li>
+          ))}
+        </ul>
 
-      <div className="navbar">
-        <Link to="/" className="nav">
-          <img
-            src={homeicon}
-            alt="home icon that brings you back to gamedev main page"
-            className="svg-icon"
-          />
-          <h3 className="home nav-item">Home</h3>
-        </Link>
-        <Link to="/about" className="nav">
-          <img
-            src={abouticon}
-            alt="icon that brings you to gamedev about us page"
-            className="svg-icon"
-          />
-          <h3 className="about nav-item">About</h3>
-        </Link>
-        <Link to="/events" className="nav">
-          <img
-            src={eventsicon}
-            alt="icon that brings you to gamedev upcoming events page"
-            className="svg-icon"
-          />
-          <h3 className="events nav-item">Events</h3>
-        </Link>
-        <Link to="/sponsor" className="nav">
-          <img
-            src={sponsorsicon}
-            alt="icon that brings you to gamedev sponsor page"
-            className="svg-icon"
-          />
-          <h3 className="sponsors nav-item">Sponsors</h3>
-        </Link>
-      </div>
-
-      <div className="socials-menu">
-        <a
-          href="https://www.instagram.com/concordiagamedev/?hl=en"
-          target="_blank"
-          className="nav-social-icons ig-icon svg"
-        >
-          <img
-            src={ig}
-            alt="ig icon that brings you to game dev concordia instagram"
-            className="menu-svg"
-          ></img>
-        </a>
-        <a
-          href="https://www.facebook.com/concordiagamedev/"
-          target="_blank"
-          className="nav-social-icons fb-icon svg"
-        >
-          <img
-            src={fb}
-            alt="ig icon that brings you to game dev concordia facebook"
-            className="menu-svg"
-          ></img>
-        </a>
-        <a
-          href="https://concordiagamedev.itch.io/"
-          target="_blank"
-          className="nav-social-icons itch-icon svg"
-        >
-          <img
-            src={itch}
-            alt="ig icon that brings you to game dev concordia itch.io"
-            className="menu-svg"
-          ></img>
-        </a>
-        <a
-          href="https://www.linkedin.com/company/concordia-game-development/?original_referer="
-          target="_blank"
-          className="nav-social-icons linked-icon svg"
-        >
-          <img
-            src={ln}
-            alt="ig icon that brings you to game dev concordia linkedin"
-            className="menu-svg"
-          ></img>
-        </a>
+        <div className="socials-menu flex-shrink-0 ">
+          <a
+            href="https://www.instagram.com/concordiagamedev/?hl=en"
+            target="_blank"
+            className="nav-social-icons ig-icon svg"
+          >
+            <img
+              src={ig}
+              alt="ig icon that brings you to game dev concordia instagram"
+              className="menu-svg"
+            ></img>
+          </a>
+          <a
+            href="https://www.facebook.com/concordiagamedev/"
+            target="_blank"
+            className="nav-social-icons fb-icon svg"
+          >
+            <img
+              src={fb}
+              alt="ig icon that brings you to game dev concordia facebook"
+              className="menu-svg"
+            ></img>
+          </a>
+          <a
+            href="https://concordiagamedev.itch.io/"
+            target="_blank"
+            className="nav-social-icons itch-icon svg"
+          >
+            <img
+              src={itch}
+              alt="ig icon that brings you to game dev concordia itch.io"
+              className="menu-svg"
+            ></img>
+          </a>
+          <a
+            href="https://www.linkedin.com/company/concordia-game-development/?original_referer="
+            target="_blank"
+            className="nav-social-icons linked-icon svg"
+          >
+            <img
+              src={ln}
+              alt="ig icon that brings you to game dev concordia linkedin"
+              className="menu-svg"
+            ></img>
+          </a>
+        </div>
       </div>
     </div>
   );
