fdcc7a766377c0c1637c6c61cf95192a88efc501
Peizhe Tian <peizhe2001@hotmail.com>
2023-10-09 22:52:21 -0400

    homepage fix responsiveness

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -16,7 +16,7 @@ export default function Index() {
   return (
     <div className="header-footer">
       <div className="page">
-        <h1 className="gameclub sm:text-8xl text-6xl pb-11">
+        <h1 className="gameclub sm:text-8xl text-5xl pb-11">
           Concordia Game Development
         </h1>
       </div>
